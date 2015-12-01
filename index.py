5da801caf68d4384e557b13aaf80400d80755bad
ibinary <ibinary@126.com>
2015-12-01 20:56:07 +0800

    template
    template

--- index.py
+++ index.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!我是主页'
+
+@app.route('/test/<title>')
+def func_test(title=None):
+    return render_template('test.html',title = title)
+
+
+
 
 if __name__ == '__main__':
+    app.debug=True
     app.run()
 
 
